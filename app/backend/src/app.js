d02444cac2afdb99192631f0c38a86b5b1e20c93
Paulino Reis <paulinoreis@naver.com>
2019-06-28 14:42:31 -0300

    003 commit

--- app/backend/src/app.js
+++ app/backend/src/app.js
@@ -24,20 +24,14 @@ app.use(bodyParser.json({ limit: '1mb' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // *** CORS ****
+
 app.use(function (req, res, next) {
-    res.header(
-        'Access-Control-Allow-Origin',
-        '*'
-    );
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, x-access-token'
-    );
-    res.header(
-        'Access-Control-Allow-Methods',
-        'GET, POST, PUT, DELETE, OPTIONS'
-    );
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    next();
 })
+
 
 
 // **** set router ****
